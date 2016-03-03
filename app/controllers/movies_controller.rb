11f9db30f59a04aa0656e5f74cb100b2d043483a
Prabhsharan Singh Brar <prabh182006@gmail.com>
2016-03-03 03:50:26 +0000

    added search tools

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.all
+    if search_params
+      str = Movie.search_string(search_params)
+      p str
+      @movies = Movie.where(str)
+    else
+      @movies = Movie.all
+    end
   end
 
   def show
@@ -48,6 +54,10 @@ class MoviesController < ApplicationController
     params.require(:movie).permit(
       :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description, :image
     )
+  end
+
+  def search_params
+    params.permit(:title, :director, :duration).delete_if { |key, value| value.blank? }
   end
 
 end
